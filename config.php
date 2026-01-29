458292066bcfb1978c8317cf4048f664b2ff3a4b
aomjk <aomjk@hotmail.com>
2026-01-29 13:48:14 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,12 @@
 <?php
 // Configurazione del database
-$db_host = '34.154.239.168'; # ricc thingy
-$db_name = 'image_catalog';
-$db_user = 'appmod-phpapp-user';
-$db_pass = '7A5q_MNmAPG8';
+$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_name = getenv('DB_NAME') ?: 'image_catalog';
+$db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
+$db_pass = getenv('DB_PASS') ?: 'wrong_password';
+
+
+
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
@@ -14,3 +17,4 @@ try {
 
 session_start();
 ?>
+
